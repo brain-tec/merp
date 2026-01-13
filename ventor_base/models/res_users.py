6c527b81ce2830a9402dd45cb6c5c0f8c6d3c4ea
kirill <kirill.v@ventor.tech>
2026-01-13 07:22:46 +0000

    [MERPWH-1688] fix conflicts

--- ventor_base/models/res_users.py
+++ ventor_base/models/res_users.py
@@ -188,12 +188,8 @@ class ResUsers(models.Model):
         result = super().write(vals)
         if result:
             self.env.registry.clear_cache()
-<<<<<<< HEAD
-        self._update_group_picking_wave_menu(vals)
-=======
 
             if warehouses_changed:
                 self.with_context(skip_allowed_wh_sync=True)._set_operation_types(previous_wh_ids_by_user)
 
->>>>>>> 3a27ecd ([MERPWH-1688] added Allowed Operation Types field and related functionality)
         return result
