7748c395ced07df6aa61768c84383fcb3dbd959a
kirill <kirill.v@ventor.tech>
2026-01-27 06:59:36 +0000

    [FX-3] added Favorite Warehouse field

--- ventor_base_fahrrad/README.rst
+++ ventor_base_fahrrad/README.rst
@@ -6,4 +6,4 @@ Changelog
 
 18.0.1.0.0 (2026-01-22)
 ***********************
-* Added "Favorite Operation Type" field for users
+* Added "Favorite Warehouse" field for users
--- ventor_base_fahrrad/models/res_users.py
+++ ventor_base_fahrrad/models/res_users.py
@@ -1,14 +1,19 @@
 ﻿# Copyright 2026 VentorTech OU
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    favorite_operation_type = fields.Many2one(
-        comodel_name="stock.picking.type",
-        string="Favorite Operation Type",
-        help="Operation type that will be used as user's preferred one in Ventor",
+    favorite_warehouse_id = fields.Many2one(
+        comodel_name="stock.warehouse",
+        string="Favorite Warehouse",
+        help="Warehouse that will be used as user's preferred one in Ventor",
     )
+
+    @api.onchange("allowed_warehouse_ids")
+    def _onchange_allowed_warehouse_ids(self):
+        if self.favorite_warehouse_id and self.favorite_warehouse_id not in self.allowed_warehouse_ids:
+            self.favorite_warehouse_id = False
