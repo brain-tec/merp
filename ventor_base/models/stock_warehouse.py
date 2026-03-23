a42786808e2e74d1ff0e31cd87f0b04055bb3fce
kirill <kirill.v@ventor.tech>
2026-03-23 07:36:57 +0000

    [CUSSUP-347] archived internal users are excluded

--- ventor_base/models/stock_warehouse.py
+++ ventor_base/models/stock_warehouse.py
@@ -20,13 +20,21 @@ class StockWarehouse(models.Model):
 
     def update_users_calculated_warehouse(self):
         for warehouse in self:
-            users = self.env['res.users'].with_context(active_test=False).search([
-                ('share', '=', False)])
+            odoo_bot = self.env.ref('base.user_root', raise_if_not_found=False)
+            default_user = self.env.ref('base.default_user', raise_if_not_found=False)
+
+            special_ids = [u.id for u in (odoo_bot, default_user) if u]
+            users = self.env['res.users'].search([
+                ('share', '=', False),
+                '|',
+                ('active', '=', True),
+                ('id', 'in', special_ids),
+            ])
             wh_ids = self.env['stock.warehouse'].with_context(active_test=False).search([
                 ('id', '!=', warehouse.id)]).ids
             wh_ids.sort()
             modified_user_ids = []
-            for user in users.with_context(active_test=False):
+            for user in users:
                 # Because of specifics on how Odoo working with companies on first start, we have to filter by company
                 user_wh_ids = user.allowed_warehouse_ids.filtered(
                     lambda wh: wh.company_id.id == warehouse.env.company.id
